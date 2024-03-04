911aefddd72e86ebfa9d0434876b73409c4efe28
Artem Safin <artemsafin.webdev@gmail.com>
2024-03-04 11:23:08 +0300

    По лоадеру

--- src/js/loader.ts
+++ src/js/loader.ts
@@ -42,5 +42,81 @@ export default function loader() {
           ease: "power2.out",
         }
       );
+
+    if (window.matchMedia("(max-width: 640px)").matches) return;
+    tl.fromTo(
+      ".page-header",
+      {
+        yPercent: -100,
+        autoAlpha: 0,
+      },
+      {
+        yPercent: 0,
+        autoAlpha: 1,
+        duration: 0.8,
+        ease: "linear",
+        clearProps: "all",
+      },
+      "<+=0.3"
+    )
+      .fromTo(
+        ".intro__heading-slider",
+        {
+          autoAlpha: 0,
+          y: 40,
+        },
+        {
+          y: 0,
+          autoAlpha: 1,
+          duration: 1,
+          ease: "power3.out",
+          clearProps: "all",
+        },
+        ">+=0.4"
+      )
+      .fromTo(
+        ".intro__main-slider",
+        {
+          autoAlpha: 0,
+          y: 40,
+        },
+        {
+          autoAlpha: 1,
+          y: 0,
+          duration: 1,
+          ease: "power3.out",
+          clearProps: "all",
+        },
+        "<+=0.3"
+      )
+      .fromTo(
+        ".intro__numbers-slider",
+        {
+          autoAlpha: 0,
+          y: 40,
+        },
+        {
+          autoAlpha: 1,
+          y: 0,
+          duration: 1,
+          ease: "power3.out",
+          clearProps: "all",
+        },
+        "<+=0.3"
+      )
+      .fromTo(
+        ".intro__navigation",
+        {
+          autoAlpha: 0,
+        },
+        {
+          autoAlpha: 1,
+
+          duration: 1,
+          ease: "power3.out",
+          clearProps: "all",
+        },
+        "<"
+      );
   }, 1000);
 }
